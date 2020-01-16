9027c217f02a57ed8866d05bcce5024d58e96151
Sander Verschoor <sanderverschoor1909@gmail.com>
2020-01-16 10:22:05 +0000

    Update config.php - Toegevoegd: Inputs door sander

--- config.php
+++ config.php
@@ -11,9 +11,32 @@ include("header.php");
             <label for="dbhost">Database host:</label>
             <input type="text" name="dbhost" class="form-control" placeholder="Vul database ip in..">
           </div>
-        </form>
-
-            
+            <div class="form-group">
+            <label for="dbusername">Database gebruikersnaam :</label>
+            <input type="text" name="dbuser" class="form-control" placeholder="Vul de database gebruikersnaam in..">
+          </div>
+            <div class="form-group">
+            <label for="dbpassword">Database wachtwoord :</label>
+            <input type="password" name="dbpassword" class="form-control" placeholder="Vul het database wachtwoord in..">
+          </div>
+            <div class="form-group">
+            <label for="dbname">Database naam :</label>
+            <input type="text" name="dbname" class="form-control" placeholder="Vul database naam in..">
+          </div>
+          <div class="form-group">
+            <label for="adres">Adres en huisnummer :</label>
+            <input type="text" name="adres" class="form-control" placeholder="Vul uw adres en huinummer in..">
+          </div>
+          <div class="form-group">
+            <label for="email">E-mailadres:</label>
+            <input type="text" name="email" class="form-control" placeholder="Vul uw e-mailadres in..">
+          </div>
+            <div class="form-group">
+            <label for="dbhost">Database Name:</label>
+            <input type="text" name="dbname" class="form-control" placeholder="Vul database naam in..">
+          </div>
+            </form>
+                    
             
             
         </div>
