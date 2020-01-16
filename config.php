1bfe019a555a7e79e2242c3cb7262d914a6c4d66
Serge Huijsen <serge@spixo.nl>
2020-01-16 10:12:31 +0000

    Update config.php

--- config.php
+++ config.php
@@ -1,3 +1,24 @@
 <?php
 
+
+
 ?>
+
+<body>
+    <div class="container">
+        <form>
+          <div class="form-group">
+            <label for="dbhost">Database host:</label>
+            <input type="text" name="dbhost" class="form-control" placeholder="Vul database ip in..">
+          </div>
+        </form>
+
+            
+            
+            
+        </div>
+        
+        
+    
+    
+</body>
