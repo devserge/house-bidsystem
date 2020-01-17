613b2e43287e6285c55f00627eeb41d927572098
Serge Huijsen <serge@spixo.nl>
2020-01-17 11:19:31 +0000

    Update setup.php - alle inputtypes required gemaakt

--- setup.php
+++ setup.php
@@ -11,19 +11,19 @@ include("header.php");
               <h1>Database gegevens:</h1>
               <div class="form-group">
                 <label for="dbhost">Database host:</label>
-                <input type="text" name="dbhost" class="form-control" placeholder="Vul database ip in..">
+                <input type="text" name="dbhost" class="form-control" placeholder="Vul database ip in.." required>
               </div>
                 <div class="form-group">
                 <label for="dbusername">Database gebruikersnaam :</label>
-                <input type="text" name="dbuser" class="form-control" placeholder="Vul de database gebruikersnaam in..">
+                <input type="text" name="dbuser" class="form-control" placeholder="Vul de database gebruikersnaam in.." required>
               </div>
                 <div class="form-group">
                 <label for="dbpassword">Database wachtwoord:</label>
-                <input type="password" name="dbpassword" class="form-control" placeholder="Vul het database wachtwoord in..">
+                <input type="password" name="dbpassword" class="form-control" placeholder="Vul het database wachtwoord in.." required>
               </div>
                 <div class="form-group">
                 <label for="dbname">Database naam:</label>
-                <input type="text" name="dbname" class="form-control" placeholder="Vul database naam in..">
+                <input type="text" name="dbname" class="form-control" placeholder="Vul database naam in.." required>
               </div>
             </div>
             <br>
@@ -31,15 +31,15 @@ include("header.php");
               <h1>Administratieve gegevens:</h1>
               <div class="form-group">
                 <label for="adres">Adres en huisnummer:</label>
-                <input type="text" name="adres" class="form-control" placeholder="Vul uw adres en huinummer in..">
+                <input type="text" name="adres" class="form-control" placeholder="Vul uw adres en huinummer in.." required>
               </div>
               <div class="form-group">
                 <label for="email">E-mailadres:</label>
-                <input type="text" name="email" class="form-control" placeholder="Vul uw e-mailadres in..">
+                <input type="text" name="email" class="form-control" placeholder="Vul uw e-mailadres in.." required>
               </div>
                 <div class="form-group">
                 <label for="huisnaam">Naam van het huis:</label>
-                <input type="text" name="huisnaam" class="form-control" placeholder="Vul hier de naam van het huis in..">
+                <input type="text" name="huisnaam" class="form-control" placeholder="Vul hier de naam van het huis in.." required>
               </div>
             </div>
           </form>
