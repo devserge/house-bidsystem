ad163fef8686775d9ea8c3bf61148827a3cfc5bc
Serge Huijsen <serge@spixo.nl>
2020-01-17 13:58:23 +0000

    Update setup.inc.php - terug naar toen het wel werkte hutsssssssss

--- includes/setup.inc.php
+++ includes/setup.inc.php
@@ -10,11 +10,11 @@ if(isset($_POST['dbconntest'])) {
     
     
     if ($conn->connect_error) {
-         echo "
+        echo "
             <div class='alert alert-danger' role='alert'>
-            Er kon geen verbinding worden gemaakt met de database. Zie bovenstaande foutcode.
-            </div>";
-            die();
+            Er kon geen verbinding worden gemaakt met de database. Zijn de gegevens juist?
+            </div>    
+        ";
     }else {
         echo "
             <div class='alert alert-success' role='alert'>
@@ -25,4 +25,3 @@ if(isset($_POST['dbconntest'])) {
 }
 
 ?>
-
