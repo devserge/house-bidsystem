e493a77dd0fe2350c7765059783963dacc1b1f08
Serge Huijsen <serge@spixo.nl>
2020-01-17 12:36:02 +0000

    Update setup.inc.php - de variabelen gedefinieerd voor de connectie later

--- includes/setup.inc.php
+++ includes/setup.inc.php
@@ -1,12 +1,27 @@
 <?php
 
 if(isset($_POST['dbconntest'])) {
-    echo "
-    <div class='alert alert-danger' role='alert'>
-    Er kon geen verbinding worden gemaakt met de database. Zijn de gegevens juist?
-    </div>    
-    ";
-
+    $dbhost = $_POST[dbhost];
+    $dbuser = $_POST[dbname];
+    $dbpass = $_POST[dbpass];
+    $dbname = $_POST[dbname];
+    
+    $conn = new mysqli($dbhost, $dbuser, $dbpass, $dbname);
+    
+    
+    if ($conn->connect_error) {
+        echo "
+            <div class='alert alert-danger' role='alert'>
+            Er kon geen verbinding worden gemaakt met de database. Zijn de gegevens juist?
+            </div>    
+        ";
+    }else {
+        echo "
+            <div class='alert alert-success' role='alert'>
+            Huts! Er is verbinding gelegd met de database!
+            </div>    
+        ";
+    }
 }
 
 ?>
