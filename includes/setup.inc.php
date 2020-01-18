eb37ba8f32119d304f9ab093b3ff1ed231a35e58
Serge Huijsen <serge@spixo.nl>
2020-01-18 14:51:27 +0000

    Update setup.inc.php - function voor opslaan met demoecho toegevoegd en de variabelen voor de eerste if gedefinieerd

--- includes/setup.inc.php
+++ includes/setup.inc.php
@@ -1,27 +1,45 @@
 <?php
 
-if(isset($_POST['dbconntest'])) {
-    $host = $_POST['dbhost'];
-    $user = $_POST['dbuser'];
-    $pass = $_POST['dbpass'];
-    $name = $_POST['dbname'];
+$host = $_POST['dbhost'];
+$user = $_POST['dbuser'];
+$pass = $_POST['dbpass'];
+$name = $_POST['dbname'];
+
+// Database connectie testen zodra op de knop gedrukt wordt
+if(isset($_POST['dbconntest'])) { //
     
+    // database connectie leggen
     $conn = new mysqli($host, $user, $pass, $name);
     
-    
+    // als de verbinding mislukt geef error
     if ($conn->connect_error) {
         echo "
             <div class='alert alert-danger' role='alert'>
             Er kon geen verbinding worden gemaakt met de database. Zijn de gegevens juist?
             </div>    
         ";
-    }else {
+    }else { // als de verbinding slaagt geef succes
         echo "
             <div class='alert alert-success' role='alert'>
-            Huts! Er is verbinding gelegd met de database!
+            De verbinding is gelukt. Je kunt verder gaan met het proces.
             </div>    
         ";
     }
 }
+
+if(isset($_POST['dbopslaan'])) {
+    echo "
+    De volgende database gegevens zijn opgeslagen:
+    Host: $host
+    Gebruiker: $user
+    Wachtwoord: $pass
+    Database: $name
+    ";
+    
+    
+}
+
+
+
 
 ?>
