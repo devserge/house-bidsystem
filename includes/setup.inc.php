38725b49cb4f6aab09a4c72acd742d580a10c00e
Serge Huijsen <serge@spixo.nl>
2020-01-21 14:43:39 +0000

    Update setup.inc.php - commentaar toegevoegd

--- includes/setup.inc.php
+++ includes/setup.inc.php
@@ -1,21 +1,23 @@
 <?php
 
+// de functions includen
 include("includes/functions.php");
 
+// naar functie als er op de button wordt gedrukt
 if(isset($_POST['dbconntest'])) {
 	testConnection();
 };
 
-
+// naar functie als er op de button wordt gedrukt
 if(isset($_POST['dbopslaan'])) {
 	saveConnection();
 };
-
+// naar functie als er op de button wordt gedrukt
 if(isset($_POST['dbopzet'])) {
 	dbOpzet();
 };
 
-
+// naar functie als er op de button wordt gedrukt
 if(isset($_POST['dataopslaan'])) {
 	saveData();
 };
